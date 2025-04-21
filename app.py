87688188f3746c543925bab060e52ede51177e25
Hamza Nawaz <hamzawatoo5251@gmail.com>
2025-04-21 00:26:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -126,3 +126,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Create site.db if it doesn’t exist
     app.run(debug=True)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
